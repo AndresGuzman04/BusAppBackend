e60312f423afc0e16eb51bd4aeb4b46869d46d5f
AndresGuzman04 <andr35guzman2001@gmail.com>
2025-01-15 22:14:31 -0600

    Create database script

---
+++ DB_SCRIPT/BusApp_SQL.sql
@@ -0,0 +1,59 @@
+DROP DATABASE IF EXISTS BusApp;
+
+CREATE DATABASE BusApp;
+
+USE BusApp;
+
+-- Crear tabla de usuarios
+CREATE TABLE users (
+    user_ID BINARY(16),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    pass VARCHAR(255) NOT NULL,
+    date_Register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Crear tabla de departamentos
+CREATE TABLE departaments (
+	departament_ID INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+-- Crear tabla de municipios
+CREATE TABLE municipalitys (
+	municipality_ID INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    departament_ID INT NOT NULL,
+    FOREIGN KEY (departament_ID) REFERENCES departaments(departament_ID)
+); 
+-- Crear tabla de ciudades
+CREATE TABLE citys (
+	city_ID INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    municipality_ID INT NOT NULL,
+    FOREIGN KEY (municipality_ID) REFERENCES municipalitys(municipality_ID)
+);
+ 
+-- Crear tabla de rutas
+CREATE TABLE routes (
+    route_ID INT AUTO_INCREMENT PRIMARY KEY,
+    name_Route VARCHAR(100) NOT NULL,
+    trip_number INT NOT NULL,
+    arrival_Time TIME NOT NULL,
+    departure_city INT NOT NULL,
+    FOREIGN KEY (departure_city) REFERENCES citys(city_ID),
+    destination_City INT NOT NULL,
+    FOREIGN KEY (destination_city) REFERENCES citys(city_ID)
+);
+
+ -- Crear paradas 
+ CREATE TABLE stops (
+	stop_ID INT AUTO_INCREMENT PRIMARY KEY,
+    num_Stop INT NOT NULL,
+    city_ID INT NOT NULL,
+    duration TIME NOT NULL,
+    route_ID INT NOT NULL,
+    description VARCHAR(255),
+    FOREIGN KEY (city_ID) REFERENCES citys(city_ID),
+    FOREIGN KEY (route_ID) REFERENCES routes(route_ID)
+ );
+
--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3000
 
 app.use('/', (req, res) => {
-    //hello world view
-    res.send('Hello World!');
+  // hello world view
+  res.send('Hello World!')
 })
 
 app.listen(PORT, () => {
-console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`)
 })
